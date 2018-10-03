669280f75452e2386a25487f466441cb5e280f31
Komal Batra <KomalBatra@gmail.com>
2018-10-02 22:03:32 -0400

    Final Version

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # Train-Scheduler
 Homework # 7
 
-Pending Tasks
- - Validate the time format 
-
 ### **Creator:** `Komal Batra`
 **Type of Project:** `Firebase`
 
-Completed on: Oct 2nd 2018
+Completed on: Sep 29th 2018
 
 - - -
 ## IMPORTANT LINKS
@@ -18,10 +15,18 @@ Completed on: Oct 2nd 2018
 - - - 
 
 ## Description of Application
-Train Schduler is a web based application that lets an admin user enter details about a train and it's frequency. Users can use this website to know the arrival time of a train. 
+Train Schduler is a web based application that lets a user enter details about a train and it's frequency. Users can use this website to know the arrival time of a train. 
 
-- Ensure frquency is a number
-- Ensure all entries are mandatory
+Involves:
+* Connecting to a Firebase database and capturing user submitted data.
+* System will calculate next arrival time and minutes remaining based on user input. 
+* New train information that is added by the user is added to the table on the website. 
+* The user can delete a train record by clicking on the delete button. 
+
+User Input Validations include:
+- Validate that all fields are completed before submitting
+- Ensure frequency is a number
+- Ensure frquency is not a negative number
 
 - - -
 ## Technologies Used
@@ -36,21 +41,3 @@ Train Schduler is a web based application that lets an admin user enter details 
 - Typography
 - Firebase
 - Moment.js
-
-- - -
-## Javascript File
-
-Validations:
-User should enter all information for Submit to work
-User can only use a number for frequency
-User can not use a negative number for frequency
-
-Involves:
-Connecting to a Firebase database and capturing user inputted data
-User can also enter the name and other details of a new train
-System will calculate next arrival time and minutes remaining based on user input. 
-Any new train information is added to the website. 
-The user can delete a train record by clicking on the delete button. 
-
-
-Challenge I could not reolve - A record is still visible on teh screen after it is deleted. Once page refreshes, then the data disapppers. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,120 +2,121 @@
 
 $(document).ready(function() {
 
-var rowNum = 0;
+  var rowNum = 0;
 
-// Initializing Firebase
-var config = {
-    apiKey: "AIzaSyD_NNRuOlWsz6iQ75WMF2CHEDwO7FTvarE",
-    authDomain: "train-scheduler-55dc1.firebaseapp.com",
-    databaseURL: "https://train-scheduler-55dc1.firebaseio.com",
-    projectId: "train-scheduler-55dc1",
-    storageBucket: "train-scheduler-55dc1.appspot.com",
-    messagingSenderId: "73169743627"
-  };
-  firebase.initializeApp(config);
+  // Initializing Firebase
+  var config = {
+      apiKey: "AIzaSyD_NNRuOlWsz6iQ75WMF2CHEDwO7FTvarE",
+      authDomain: "train-scheduler-55dc1.firebaseapp.com",
+      databaseURL: "https://train-scheduler-55dc1.firebaseio.com",
+      projectId: "train-scheduler-55dc1",
+      storageBucket: "train-scheduler-55dc1.appspot.com",
+      messagingSenderId: "73169743627"
+    };
+    firebase.initializeApp(config);
 
-  var database = firebase.database();
-  
-// Adds data to firebase when Submit button is clicked
-$('#add-train-btn').on('click', function(event){
-    event.preventDefault();
-    var trainName = $('#train-input').val().trim();
-    var destination = $('#dest-input').val().trim();
-    var firstTime = $('#time-input').val().trim();
-    var frequency = $('#freq-input').val().trim();
-    // Validate if fields are left blank
-    if((trainName.length<1) ||(destination.length<1) || (firstTime.length<1) || (frequency.length<1)){
-      // Warn user with Modal
-      $('.modal-2').modal();
-    }
-    // validate if frequncy is less than 1
-    else if (frequency<0){
-      // warn user of error with Modal
-      $('.modal-1').modal();
+    var database = firebase.database();
+    
+  // Adds data to firebase when Submit button is clicked
+  $('#add-train-btn').on('click', function(event){
+      event.preventDefault();
+      var trainName = $('#train-input').val().trim();
+      var destination = $('#dest-input').val().trim();
+      var firstTime = $('#time-input').val().trim();
+      var frequency = $('#freq-input').val().trim();
+      // Validate if fields are left blank
+      if((trainName.length<1) ||(destination.length<1) || (firstTime.length<1) || (frequency.length<1)){
+        // Warn user with Modal
+        $('.modal-2').modal();
+      }
+      // validate if frequncy is a negative number
+      else if (frequency<0){
+        // warn user of error with Modal
+        $('.modal-1').modal();
+        $('#freq-input').val('');
+      }
+      else {
+      // store data in firebase
+      var newTrain = {
+          trainName: trainName,
+          destination: destination,
+          firstTime: firstTime,
+          frequency: frequency,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+      }
+      
+      database.ref().push(newTrain);
+
+      // modal to let user know that new train has been added.
+      $('.modal-3').modal();
+
+      // Clear Form
+      $('#train-input').val('');
+      $('#dest-input').val('');
+      $('#time-input').val('');
       $('#freq-input').val('');
-    }
-    else {
-    // store data in firebase
-    var newTrain = {
-        trainName: trainName,
-        destination: destination,
-        firstTime: firstTime,
-        frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-    }
-    
-    database.ref().push(newTrain);
-
-    // modal to let user know that new train has been added.
-    $('.modal-3').modal();
-
-    // Clear Form
-    $('#train-input').val('');
-    $('#dest-input').val('');
-    $('#time-input').val('');
-    $('#freq-input').val('');
-    }
-  return false;
-});
-
-// Adding the date to the Train Scheulde Table
-database.ref().on('child_added', function(snapshot) {
-  // Creating remove button
-  var removeButton = $('<button>').text('X').addClass('removeButton btn btn-danger btn-sm').attr('data-index', rowNum).attr('data-key', snapshot.key);
-  // storing the snapshot.val() in a variable 
-  var addTrain = snapshot.val();
-  var firstTimeConverted = moment(addTrain.firstTime, 'HH:mm').subtract(1, 'years');
-   
-  // Current Time
-  var currentTime = moment();
-  
-  // Difference between the times
-  var diffTime = moment().diff(moment(firstTimeConverted), 'minutes');
-    
-  // Time apart (remainder)
-  var tRemainder = diffTime % addTrain.frequency;
-   
-  // Minute Until Train
-  var tMinutesTillTrain = addTrain.frequency - tRemainder;
-   
-  // Next Train
-  var nextTrain = moment().add(tMinutesTillTrain, 'minutes').format('hh:mm A');
-   
-  // Change the HTML to reflect
-  var newRow = $('<tr>');
-  newRow.attr('id','row-' + rowNum);
-  var cell1 = $('<td>').text(addTrain.trainName);
-  var cell2 = $('<td>').text(addTrain.destination);
-  var cell3 = $('<td>').text(addTrain.frequency);
-  var cell4 = $('<td>').text(nextTrain);
-  var cell5 = $('<td>').text(tMinutesTillTrain);
-  var cell6 = $('<td>').html(removeButton);
-
-  newRow.append(cell1);
-  newRow.append(cell2);
-  newRow.append(cell3);
-  newRow.append(cell4);
-  newRow.append(cell5);
-  newRow.append(cell6);
-
-  $('#train-table').prepend(newRow);
-
-  rowNum++;
-
-  // Handle the errors
-  }, function(errorObject) {
-    console.log('Errors handled: ' + errorObject.code);
+      }
+    return false;
   });
 
-  // function to delete records
-  function removeTrain () {
-   // event.preventDefault();
-    $('.row-' + $(this).attr('data-index')).remove();
-    database.ref().child($(this).attr('data-key')).remove();
-  };
+  // Adding the data to the Train Schedule Table
+  database.ref().on('child_added', function(snapshot) {
+    // Creating remove button
+    var removeButton = $('<button>').text('X').addClass('removeButton btn btn-danger btn-sm').attr('data-index', rowNum).attr('data-key', snapshot.key);
+    // storing the snapshot.val() in a variable 
+    var addTrain = snapshot.val();
+    var firstTimeConverted = moment(addTrain.firstTime, 'HH:mm').subtract(1, 'years');
+    
+    // Current Time
+    var currentTime = moment();
+    
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), 'minutes');
+      
+    // Time apart (remainder)
+    var tRemainder = diffTime % addTrain.frequency;
+    
+    // Minutes Until Train
+    var tMinutesTillTrain = addTrain.frequency - tRemainder;
+    
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, 'minutes').format('hh:mm A');
+    
+    // Change the HTML to reflect
+    var newRow = $('<tr>');
+    newRow.attr('id','row-' + rowNum);
+    var cell1 = $('<td>').text(addTrain.trainName);
+    var cell2 = $('<td>').text(addTrain.destination);
+    var cell3 = $('<td>').text(addTrain.frequency);
+    var cell4 = $('<td>').text(nextTrain);
+    var cell5 = $('<td>').text(tMinutesTillTrain);
+    var cell6 = $('<td>').html(removeButton);
 
-  // event listener - when user clicks the X button to remove train
-  $(document).on('click', '.removeButton', removeTrain);
+    newRow.append(cell1);
+    newRow.append(cell2);
+    newRow.append(cell3);
+    newRow.append(cell4);
+    newRow.append(cell5);
+    newRow.append(cell6);
+
+    $('#train-table').prepend(newRow);
+
+    rowNum++;
+
+    // Handle the errors
+    }, function(errorObject) {
+      console.log('Errors handled: ' + errorObject.code);
+    });
+
+    // Function to delete records
+    function removeTrain () {
+      var index = $(this).attr('data-index');
+      var $elem = $('#row-' + index);
+      $elem.remove();
+      database.ref().child($(this).attr('data-key')).remove();
+    };
+
+    // event listener - when user clicks the X button to remove train
+    $(document).on('click', '.removeButton', removeTrain);
 
 }); //PAGE CLOSING BRACKET
